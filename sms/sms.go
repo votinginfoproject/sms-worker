bec141a8cbbf2fbd986a30d82b6ec614f73e5dbe
Joshua Antonishen <joshua@antonishen.com>
2014-08-20 10:01:11 -0500

    add sms sender

---
+++ sms/sms.go
@@ -0,0 +1,37 @@
+package sms
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+type ExternalSmsServce interface {
+	Send(message string, to string) error
+}
+
+type Twilio struct {
+	endpoint *url.URL
+	from     string
+}
+
+func New(sid string, token string, from string) *Twilio {
+	endpoint, _ := url.Parse(fmt.Sprintf("https://%s:%s@api.twilio.com", sid, token))
+	endpoint.Path += fmt.Sprintf("/2010-04-01/Accounts/%s/Messages", sid)
+
+	return &Twilio{endpoint, from}
+}
+
+func (t *Twilio) Send(message string, to string) error {
+	data := url.Values{}
+	data.Set("From", t.from)
+	data.Set("To", to)
+	data.Set("Body", message)
+
+	_, err := http.PostForm(t.endpoint.String(), data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
