443447406b4fe658effa1e44cde2c8edc7488a69
Joshua Antonishen <joshua@antonishen.com>
2014-09-22 15:46:01 -0500

    move polling location tests to their own file

--- response_generator/polling_location/polling_location_test.go
+++ response_generator/polling_location/polling_location_test.go
@@ -1,4 +1,4 @@
-package responseGenerator
+package pollingLocation_test
 
 import (
 	"errors"
@@ -7,16 +7,17 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/votinginfoproject/sms-worker/civic_api"
+	"github.com/votinginfoproject/sms-worker/response_generator"
 )
 
 var makeRequestSuccess = func(endpoint string) ([]byte, error) {
-	data, _ := ioutil.ReadFile("../civic_api/test_data/google_civic_success.json")
+	data, _ := ioutil.ReadFile("../../civic_api/test_data/google_civic_success.json")
 
 	return data, nil
 }
 
 var makeRequestError = func(endpoint string) ([]byte, error) {
-	data, _ := ioutil.ReadFile("../civic_api/test_data/google_civic_error.json")
+	data, _ := ioutil.ReadFile("../../civic_api/test_data/google_civic_error.json")
 
 	return data, nil
 }
@@ -25,25 +26,25 @@ var makeRequestFailure = func(endpoint string) ([]byte, error) {
 	return nil, errors.New("something bad has happened")
 }
 
-func TestResponsePollingLocationSuccess(t *testing.T) {
+func TesPollingLocationSuccess(t *testing.T) {
 	c := civicApi.New("", "", makeRequestSuccess)
-	g := New(c)
+	g := responseGenerator.New(c)
 
 	expected := "Your polling place is:\nSun Valley Neighborhood Center\n115 W 6th St\nSun Valley, NV 00000\nHours: 7am-7pm"
 	assert.Equal(t, expected, g.Generate(""))
 }
 
-func TestResponsePollingLocationError(t *testing.T) {
+func TestPollingLocationError(t *testing.T) {
 	c := civicApi.New("", "", makeRequestError)
-	g := New(c)
+	g := responseGenerator.New(c)
 
 	expected := "the civic api returned an error"
 	assert.Equal(t, expected, g.Generate(""))
 }
 
-func TestResponsePollingLocationFailure(t *testing.T) {
+func TestPollingLocationFailure(t *testing.T) {
 	c := civicApi.New("", "", makeRequestFailure)
-	g := New(c)
+	g := responseGenerator.New(c)
 
 	expected := "an error has occurred"
 	assert.Equal(t, expected, g.Generate(""))
