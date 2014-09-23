a8ac948b4f17e6555e966aca1b3af68e90eda18f
Joshua Antonishen <joshua@antonishen.com>
2014-09-23 12:32:46 -0500

    build a lookup for trigger -> command by language

--- responses/responses.go
+++ responses/responses.go
@@ -3,6 +3,7 @@ package responses
 import (
 	"log"
 
+	"github.com/fatih/structs"
 	"gopkg.in/yaml.v1"
 )
 
@@ -16,7 +17,7 @@ type Responses struct {
 	ElectionOfficial Response "electionOfficial"
 }
 
-func Load(raw []byte) *Responses {
+func Load(raw []byte) (*Responses, map[string]map[string]string) {
 	r := &Responses{}
 
 	err := yaml.Unmarshal(raw, r)
@@ -24,5 +25,26 @@ func Load(raw []byte) *Responses {
 		log.Panic("[ERROR] Failed to parse responses : ", err)
 	}
 
-	return r
+	return r, buildTriggerLookup(r)
+}
+
+func buildTriggerLookup(r *Responses) map[string]map[string]string {
+	lookup := make(map[string]map[string]string)
+
+	for _, field := range structs.New(r).Fields() {
+		name := field.Name()
+
+		triggers := field.Value().(Response).Triggers
+
+		for language, words := range triggers {
+			if lookup[language] == nil {
+				lookup[language] = make(map[string]string)
+			}
+			for _, word := range words {
+				lookup[language][word] = name
+			}
+		}
+	}
+
+	return lookup
 }
--- responses/responses_test.go
+++ responses/responses_test.go
@@ -14,9 +14,18 @@ func loadTestData() []byte {
 }
 
 func TestParseYaml(t *testing.T) {
-	d := Load(loadTestData())
+	d, _ := Load(loadTestData())
 	assert.Equal(t, "testa", d.PollingLocation.Text["en"]["first"])
 	assert.Equal(t, "atest", d.PollingLocation.Text["es"]["first"])
 	assert.Equal(t, 2, len(d.PollingLocation.Triggers["en"]))
 	assert.Equal(t, 2, len(d.PollingLocation.Triggers["es"]))
+}
+
+func TestBuildTriggerLookup(t *testing.T) {
+	_, l := Load(loadTestData())
+	assert.Equal(t, "PollingLocation", l["es"]["uno"])
+	assert.Equal(t, "PollingLocation", l["en"]["one"])
+	assert.Equal(t, "ElectionOfficial", l["es"]["tres"])
+	assert.Equal(t, "ElectionOfficial", l["en"]["three"])
+	assert.Equal(t, "", l["en"]["nope"])
 }
