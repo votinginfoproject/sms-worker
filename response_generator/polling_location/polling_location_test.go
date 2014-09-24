a4d5ee0ff67124627d402c6e6ced71f440877444
Joshua Antonishen <joshua@antonishen.com>
2014-09-24 12:54:39 -0500

    test with existing user

--- response_generator/polling_location/polling_location_test.go
+++ response_generator/polling_location/polling_location_test.go
@@ -4,7 +4,9 @@ import (
 	"errors"
 	"io/ioutil"
 	"log"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/votinginfoproject/sms-worker/civic_api"
@@ -33,7 +35,7 @@ var makeRequestFailure = func(endpoint string) ([]byte, error) {
 	return nil, errors.New("something bad has happened")
 }
 
-func TestPollingLocationSuccess(t *testing.T) {
+func TestPollingLocationSuccessNewUser(t *testing.T) {
 	setup()
 	s := fakeStorage.New()
 	u := users.New(s)
@@ -42,7 +44,24 @@ func TestPollingLocationSuccess(t *testing.T) {
 	g := responseGenerator.New(c)
 
 	expected := []string{"Your polling place is:\nSun Valley Neighborhood Center\n115 W 6th St\nSun Valley, NV 00000\nHours: 7am-7pm"}
-	assert.Equal(t, expected, g.Generate(u, "", "", 0))
+	assert.Equal(t, expected, g.Generate(u, "+15551235555", "", 0))
+}
+
+func TestPollingLocationSuccessExistingUser(t *testing.T) {
+	setup()
+	s := fakeStorage.New()
+
+	time := time.Now().Unix()
+	timeString := strconv.FormatInt(time, 10)
+	s.CreateItem("+15551235555", map[string]string{"language": "es", "last_contact": timeString})
+
+	u := users.New(s)
+
+	c := civicApi.New("", "", makeRequestSuccess)
+	g := responseGenerator.New(c)
+
+	expected := []string{"spanish-Your polling place is:\nSun Valley Neighborhood Center\n115 W 6th St\nSun Valley, NV 00000\nHours: 7am-7pm"}
+	assert.Equal(t, expected, g.Generate(u, "+15551235555", "", 0))
 }
 
 func TestPollingLocationError(t *testing.T) {
@@ -54,7 +73,7 @@ func TestPollingLocationError(t *testing.T) {
 	g := responseGenerator.New(c)
 
 	expected := []string{"That isn’t a recognized command. Text HELP to see all options."}
-	assert.Equal(t, expected, g.Generate(u, "", "", 0))
+	assert.Equal(t, expected, g.Generate(u, "+15551235555", "", 0))
 }
 
 func TestPollingLocationFailure(t *testing.T) {
@@ -66,5 +85,5 @@ func TestPollingLocationFailure(t *testing.T) {
 	g := responseGenerator.New(c)
 
 	expected := []string{"Sorry, we were unable to find your election day polling location."}
-	assert.Equal(t, expected, g.Generate(u, "", "", 0))
+	assert.Equal(t, expected, g.Generate(u, "+15551235555", "", 0))
 }
