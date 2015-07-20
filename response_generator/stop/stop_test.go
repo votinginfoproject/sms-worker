950051393e7cdbf6443ee7726e078e34c0c02610
Chris Shea <cmshea@gmail.com>
2015-07-20 15:38:39 -0400

    [CS] Add test for STOP command

---
+++ response_generator/stop/stop_test.go
@@ -0,0 +1,39 @@
+package stop_test
+
+import (
+	"io/ioutil"
+	"log"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/votinginfoproject/sms-worker/civic_api"
+	"github.com/votinginfoproject/sms-worker/civic_api/fixtures"
+	"github.com/votinginfoproject/sms-worker/fake_storage"
+	"github.com/votinginfoproject/sms-worker/response_generator"
+	"github.com/votinginfoproject/sms-worker/test_helpers"
+	"github.com/votinginfoproject/sms-worker/users"
+)
+
+func setup() {
+	log.SetOutput(ioutil.Discard)
+}
+
+var content = testHelpers.GetContent()
+
+func TestStopWithCommand(t *testing.T) {
+	setup()
+	s := fakeStorage.New()
+	u := users.New(s)
+
+	time := time.Now().Unix()
+	timeString := strconv.FormatInt(time, 10)
+	s.CreateItem("+15551235555", map[string]string{"language": "en", "last_contact": timeString})
+
+	c := civicApi.New("", "", "", civicApiFixtures.MakeRequestSuccessFake)
+	g := responseGenerator.New(c, u)
+
+	expected := []string{content.Stop.Text["en"]["all"]}
+	assert.Equal(t, expected, g.Generate("+15551235555", "stop", 0))
+}
